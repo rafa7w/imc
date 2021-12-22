613a0f5614a7d067cb2b95c0d649d42389c622b3
Rafael William <rafael.fim@hotmail.com>
2021-12-22 20:24:44 -0300

    added text fields

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
     home: Home(),
   ));
 }
@@ -20,8 +21,33 @@ class _HomeState extends State<Home> {
       appBar: AppBar(
         title: const Text('Calculadora de IMC'),
         centerTitle: true,
-        backgroundColor: Colors.green,
-        actions: [IconButton(onPressed: () {}, icon: const Icon(Icons.refresh))],
+        backgroundColor: Colors.red,
+        actions: [
+          IconButton(onPressed: () {}, icon: const Icon(Icons.refresh))
+        ],
+      ),
+      backgroundColor: Colors.white,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: const [
+          Icon(Icons.person_outline, size: 120, color: Colors.red),
+          TextField(
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+                labelText: 'Peso (kg)',
+                labelStyle: TextStyle(color: Colors.red)),
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.red, fontSize: 25),
+          ),
+          TextField(
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+                labelText: 'Altura (cm)',
+                labelStyle: TextStyle(color: Colors.red)),
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.red, fontSize: 25),
+          )
+        ],
       ),
     );
   }
