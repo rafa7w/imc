df834cf8e7b0837d8cdec9974598351ff16bf771
Rafael William <rafael.fim@hotmail.com>
2021-12-23 20:32:05 -0300

    calculate imc feature

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,6 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-
   TextEditingController weightController = TextEditingController();
   TextEditingController heightController = TextEditingController();
 
@@ -29,6 +28,27 @@ class _HomeState extends State<Home> {
     _infoText = 'Informe seus dados!';
   }
 
+  void _calculate() {
+    setState(() {
+      double weight = double.parse(weightController.text);
+      double height = double.parse(heightController.text) / 100;
+      double imc = weight / (height * height);
+
+      if (imc < 18.6) {
+        _infoText = 'Abaixo do Peso (${imc.toStringAsPrecision(4)})';
+      } else if (imc >= 18.6 && imc < 24.9) {
+        _infoText = 'Peso Ideal (${imc.toStringAsPrecision(4)})';
+      } else if (imc >= 24.9 && imc < 29.9) {
+        _infoText = 'Levemente Acima do Peso (${imc.toStringAsPrecision(4)})';
+      } else if (imc >= 29.9 && imc < 34.9) {
+        _infoText = 'Obesidade Grau I (${imc.toStringAsPrecision(4)})';
+      } else if (imc >= 34.9 && imc < 39.9) {
+        _infoText = 'Obesidade Grau II (${imc.toStringAsPrecision(4)})';
+      } else if (imc >= 40) {
+        _infoText = 'Obesidade Grau III (${imc.toStringAsPrecision(4)})';
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -73,9 +93,7 @@ class _HomeState extends State<Home> {
                 // ignore: prefer_const_constructors, deprecated_member_use
                 child: RaisedButton(
                   color: Colors.brown,
-                  onPressed: () {
-                    print('click');
-                  },
+                  onPressed: _calculate,
                   child: const Text(
                     'Calcular',
                     style: TextStyle(
